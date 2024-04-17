920ce7c19fe20309879ee40516b68325075797e9
toyger-cat <toyger-cat@outlook.com>
2024-04-17 18:03:03 +0800

    Lagrange

--- Lagrange polynomial/main.c
+++ Lagrange polynomial/main.c
@@ -1,79 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Lagrange polynomial
-int n;
-double *x, *y, *param, *temp, t1, t2;
-void Lagrange();
-void l(int j);
+typedef struct {
+  int n;
+  double *x, *y, *param, *temp, t1, t2;
+} Polynomial;
+
+void Lagrange(Polynomial *p);
+void l(Polynomial *p, int j);
+void readInput(Polynomial *p);
+void evaluatePolynomial(Polynomial *p);
+
 int main() {
-  printf("Enter the number of points:");
-  scanf("%d", &n);
-  x = (double *)malloc(n * sizeof(double));
-  y = (double *)malloc(n * sizeof(double));
-  temp = (double *)malloc(n * sizeof(double));
-  param = (double *)calloc(n, sizeof(double));
-  printf("Enter x:");
-  for (int i = 0; i < n; i++) {
-    scanf("%lf", x + i);
-  }
-  printf("Enter y:");
-  for (int i = 0; i < n; i++) {
-    scanf("%lf", y + i);
-  }
-  Lagrange();
+  Polynomial p;
+  readInput(&p);
+  Lagrange(&p);
   while (1) {
-    printf("CTRL+C to exit or Enter a point to evaluate:");
-    scanf("%lf", &t1);
-    for (int i = 0; i < n; i++) {
-      t2 = 0;
-      double t3 = 1;
-      for (int j = 0; j < i; j++) {
-        t3 *= t1;
-      }
-      t2 += param[i] * t3;
-    }
-    printf("The value of the polynomial at %lf is %lf\n", t1, t2);
+    evaluatePolynomial(&p);
   }
   return 0;
 }
-void l(int j) {
-  for (int i = 0; i < n; i++) {
-    temp[i] = 0;
+
+void readInput(Polynomial *p) {
+  printf("Enter the number of points:");
+  scanf("%d", &(p->n));
+  p->x = (double *)malloc(p->n * sizeof(double));
+  p->y = (double *)malloc(p->n * sizeof(double));
+  p->temp = (double *)malloc(p->n * sizeof(double));
+  p->param = (double *)calloc(p->n, sizeof(double));
+  printf("Enter x:");
+  for (int i = 0; i < p->n; i++) {
+    scanf("%lf", p->x + i);
   }
-  t1 = temp[0] = 1.0;
-  int count = 0;
-  for (int i = 0; i < n; i++) {
-    if (i == j)
-      continue;
-    t1 *= (1.0 / (x[j] - x[i]));
-    t2 = -x[i];
-    temp[count + 1] = 1;
-    int k = count;
-    for (; k > 0; k--) {
-      temp[k] = temp[k - 1] + t2 * temp[k];
-    }
-    temp[0] = t2 * temp[0];
-    count++;
-  }
-  for (int i = 0; i < n; i++) {
-    temp[i] *= t1;
+  printf("Enter y:");
+  for (int i = 0; i < p->n; i++) {
+    scanf("%lf", p->y + i);
   }
 }
-void Lagrange() {
-  for (int i = 0; i < n; i++) {
-    l(i);
-    for (int j = 0; j < n; j++) {
-      param[j] += y[i] * temp[j];
+
+void evaluatePolynomial(Polynomial *p) {
+  printf("CTRL+C to exit or Enter a point to evaluate:");
+  scanf("%lf", &(p->t1));
+  for (int i = 0; i < p->n; i++) {
+    p->t2 = 0;
+    double t3 = 1;
+    for (int j = 0; j < i; j++) {
+      t3 *= p->t1;
+    }
+    p->t2 += p->param[i] * t3;
+  }
+  printf("The value of the polynomial at %lf is %lf\n", p->t1, p->t2);
+}
+
+void l(Polynomial *p, int j) {
+  for (int i = 0; i < p->n; i++) {
+    p->temp[i] = 0;
+  }
+  p->t1 = p->temp[0] = 1.0;
+  int count = 0;
+  for (int i = 0; i < p->n; i++) {
+    if (i == j)
+      continue;
+    p->t1 *= (1.0 / (p->x[j] - p->x[i]));
+    p->t2 = -p->x[i];
+    p->temp[count + 1] = 1;
+    int k = count;
+    for (; k > 0; k--) {
+      p->temp[k] = p->temp[k - 1] + p->t2 * p->temp[k];
+    }
+    p->temp[0] = p->t2 * p->temp[0];
+    count++;
+  }
+  for (int i = 0; i < p->n; i++) {
+    p->temp[i] *= p->t1;
+  }
+}
+
+void Lagrange(Polynomial *p) {
+  for (int i = 0; i < p->n; i++) {
+    l(p, i);
+    for (int j = 0; j < p->n; j++) {
+      p->param[j] += p->y[i] * p->temp[j];
     }
   }
   printf("\nThe Lagrange polynomial is:\n");
-  for (int i = 0; i < n; i++) {
-    printf("%lf", param[i]);
+  for (int i = 0; i < p->n; i++) {
+    printf("%lf", p->param[i]);
     if (i != 0) {
       printf("x^%d", i);
     }
-    if (i != n - 1) {
+    if (i != p->n - 1) {
       printf("+");
     }
   }
