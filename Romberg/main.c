e8a3bad33fee46f5966202655c3c540cba755a02
Charles Frazier <charlesfrazier@Charless-MacBook-Pro.local>
2024-05-02 13:07:26 +0800

    romberg

--- Romberg/main.c
+++ Romberg/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #define MAX 5000
 
 typedef struct {
@@ -25,14 +24,18 @@ void Romberg(Polynomial *r){
     r->k = 0;
     r->T = (double *)calloc(MAX, sizeof(double));
     r->T[0] = 0.5 * (r->b - r->a)*(f(r->a) + f(r->b));
+    printf("%lf\n", r->T[0]);
     do{
         r->k++;
         r->T[trans(0,r->k)] = 0.5 * r->T[trans(0,r->k-1)] + 0.5 * (r->b-r->a)/two(r->k-1) * sum(r, two(r->k-1));
+        printf("%lf ", r->T[trans(0,r->k)]);
         for(int j=1;j<=r->k;j++){
             double tmp = (double)two(2*j);
             r->T[trans(j,r->k-j)] = tmp/(tmp-1) * r->T[trans(j-1,r->k-j+1)] - 1/(tmp-1)*r->T[trans(j-1,r->k-j)];
+            printf("%lf ", r->T[trans(j,r->k-j)]);
         }
-    }while(absolute(r->T[trans(r->k,0)]-r->T[trans(r->k-1,0)])<r->epsilon);
+        printf("\n");
+    }while(absolute(r->T[trans(r->k,0)]-r->T[trans(r->k-1,0)])>r->epsilon);
     printf("The result is %lf\n", r->T[trans(r->k,0)]);
 }
 double absolute(double x){
@@ -50,11 +53,10 @@ int trans(int d, int u){
     return (d+u)*(d+u+1)/2+d;
 }
 int two(int k){
-    if(k==0)return 1;
-    return 2*two(k-1);
+    return 1<<k;
 }
 double f(double x) {
-    return x*x;
+    return 1.0/x;
 }
 void readInput(Polynomial *r) {
     printf("Enter a:");
