27019811829eb043459f2bf6930f9e3db0377075
Charles Frazier <charlesfrazier@Charless-MacBook-Pro.local>
2024-05-10 12:10:38 +0800

    roberg

--- Romberg/main.c
+++ Romberg/main.c
@@ -6,15 +6,14 @@ long double f2(long double x){ return exp(-x*x); }
 long double f3(long double x){ if(x==0)return 1; return log(1+x)/x; }
 long double (*funcPtr[3])(long double) = {f1, f2, f3};
 long double range[3][2] = {{0.1, 4}, {-2, 4}, {0, 1}};
-long double Eps = 1e-7;
+const long double EPSILON = 1e-7;
+const int MAX_ITERATIONS = 100;
 
 long double composite_trapezoidal(long double a, long double b, int n, long double (*f)(long double)){
     long double h = (b - a) / n;
     long double sum = 0.5 * (f(a) + f(b));
-    long double x;
     for (int i = 1; i < n; i++) {
-        x = a + i * h;
-        sum += f(x);
+        sum += f(a + i * h);
     }
     return h * sum;
 }
@@ -22,12 +21,12 @@ long double composite_trapezoidal(long double a, long double b, int n, long doub
 long double composite_simpson(long double a, long double b, int n, long double (*f)(long double)){
     long double h = (b - a) / n;
     long double sum = f(a) + f(b);
-    long double x;
-    for (int i = 1; i < n; i++) {
-        x = a + i * h;
-        sum += 4 * f(x + 0.5 * h) + 2 * f(x);
+    for (int i = 0; i < n; i++) {
+        sum += 4 * f(a + (i + 0.5) * h);
     }
-    sum += 4 * f(a + 0.5 * h);
+    for(int i = 1; i < n; i++) {
+        sum += 2 * f(a + i * h);
+    }
     return h / 6 * sum;
 }
 
@@ -35,16 +34,14 @@ void calculate_integral(long double a, long double b, long double (*f)(long doub
     int n = 1;
     long double result = method(a, b, n, f);
     long double new_result;
-
-    while (1) {
+    do {
+        n = n << 1;
         new_result = method(a, b, 2 * n, f);
-        if (fabs(result - new_result) < Eps) break;
-        n *= 2;
         result = new_result;
-    }
-    printf("积分结果: %.8Lf\n", result);
-    printf("区间个数: %d\n", n);
-    printf("基点个数: %d\n", n + 1);
+    } while (fabsl(result - new_result) >= EPSILON);
+    printf("积分结果: %.8Lf\n", new_result);
+    printf("区间个数: %d\n", n << 1);
+    printf("基点个数: %d\n", (n << 1) + 1);
 }
 
 int main(){
@@ -60,11 +57,11 @@ int main(){
         calculate_integral(range[i][0], range[i][1], funcPtr[i], composite_simpson);
 
         printf("\n龙贝格积分法：\n");
-        long double T[2][100];
+        long double T[2][MAX_ITERATIONS];
         T[0][0] = composite_trapezoidal(range[i][0], range[i][1], 1, funcPtr[i]);
         printf("T[0][0] = %.8Lf\n", T[0][0]);
         long double pow2j, pow4k;
-        for (int j = 1; j < 100; j++) {
+        for (int j = 1; j < MAX_ITERATIONS; j++) {
             pow2j = 1 << j;
             T[1][0] = composite_trapezoidal(range[i][0], range[i][1], pow2j, funcPtr[i]);
             printf("T[%d][0] = %.8Lf ", j, T[1][0]);
@@ -74,7 +71,7 @@ int main(){
                 printf("T[%d][%d] = %.8Lf ", j, k, T[1][k]);
             }
             printf("\n");
-            if (fabs(T[1][j] - T[0][j - 1]) < Eps) {
+            if (fabsl(T[1][j] - T[0][j - 1]) < EPSILON) {
                 printf("积分结果: %.8Lf\n\n", T[1][j]);
                 break;
             }
