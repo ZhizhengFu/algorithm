63f40c9c6ef9ab9519a65821aed65d7b8a19caa5
Charles Frazier <charles-frazier@outlook.com>
2024-05-17 09:10:41 +0800

    rust_fix

--- Romberg/main.rs
+++ Romberg/main.rs
@@ -14,6 +14,7 @@ fn f3(x: f64) -> f64 {
         1.0
     }
 }
+
 fn trapezoidal(a: f64, b: f64, n: i32, f: fn(f64) -> f64) -> f64 {
     let h = (b - a) / (n as f64);
     let mut sum = 0.5 * (f(a) + f(b));
@@ -30,6 +31,7 @@ fn simpson(a: f64, b: f64, n: i32, f: fn(f64) -> f64) -> f64 {
     }
     h / 6.0 * sum
 }
+
 fn integral(a: f64, b: f64, f: fn(f64) -> f64, method: fn(f64, f64, i32, fn(f64) -> f64) -> f64) {
     let mut result = method(a, b, 1, f);
     let mut i = 1;
@@ -55,6 +57,32 @@ fn integral(a: f64, b: f64, f: fn(f64) -> f64, method: fn(f64, f64, i32, fn(f64)
         result, ans1, ans2
     );
 }
+
+fn romberg_integration(a: f64, b: f64, f: fn(f64) -> f64) -> f64 {
+    let mut t = [[0.0; MAX_ITERATIONS]; 2];
+    t[0][0] = trapezoidal(a, b, 1, f);
+    println!("T[0][0] = {:.8}", t[0][0]);
+    for j in 1..MAX_ITERATIONS {
+        t[1][0] = trapezoidal(a, b, 1 << j, f);
+        print!("T[{}][0] = {:.8} ", j, t[1][0]);
+        for k in 1..=j {
+            t[1][k] = t[1][k - 1] + (t[1][k - 1] - t[0][k - 1]) / ((1 << (2 * k)) as f64 - 1.0);
+            print!("T[{}][{}] = {:.8} ", j, k, t[1][k]);
+        }
+        println!();
+        if (t[1][j].abs() <= 100.0 && (t[1][j] - t[0][j - 1]).abs() < EPSILON)
+            || (t[1][j].abs() > 100.0 && ((t[1][j] - t[0][j - 1]) / t[1][j]).abs() < EPSILON)
+        {
+            println!("积分结果: {:.8}\n", t[1][j]);
+            return t[1][j];
+        }
+        for k in 0..=j {
+            t[0][k] = t[1][k];
+        }
+    }
+    t[1][MAX_ITERATIONS - 1]
+}
+
 fn main() {
     let func_ptr: [fn(f64) -> f64; 3] = [f1, f2, f3];
     let range: [[f64; 2]; 3] = [[0.1, 4.0], [-2.0, 4.0], [0.0, 1.0]];
@@ -73,26 +101,6 @@ fn main() {
         println!("\n复合Simpson公式：");
         integral(start, end, func_ptr[i], simpson);
         println!("\n龙贝格积分法：");
-        let mut t = [[0.0; MAX_ITERATIONS]; 2];
-        t[0][0] = trapezoidal(start, end, 1, func_ptr[i]);
-        println!("T[0][0] = {:.8}", t[0][0]);
-        for j in 1..MAX_ITERATIONS {
-            t[1][0] = trapezoidal(start, end, 1 << j, func_ptr[i]);
-            print!("T[{}][0] = {:.8} ", j, t[1][0]);
-            for k in 1..=j {
-                t[1][k] = t[1][k - 1] + (t[1][k - 1] - t[0][k - 1]) / ((1 << (2 * k)) as f64 - 1.0);
-                print!("T[{}][{}] = {:.8} ", j, k, t[1][k]);
-            }
-            println!();
-            if (t[1][j].abs() <= 100.0 && (t[1][j] - t[0][j - 1]).abs() < EPSILON)
-                || (t[1][j].abs() > 100.0 && ((t[1][j] - t[0][j - 1]) / t[1][j]).abs() < EPSILON)
-            {
-                println!("积分结果: {:.8}\n", t[1][j]);
-                break;
-            }
-            for k in 0..=j {
-                t[0][k] = t[1][k];
-            }
-        }
+        romberg_integration(start, end, func_ptr[i]);
     }
 }
