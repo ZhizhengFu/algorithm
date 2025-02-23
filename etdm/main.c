7963b9a397877b0c0a0197be2f2f91fca6edfb32
Zhizheng Fu <zhizheng.foo@gmail.com>
2025-02-23 17:53:43 +0800

    Update main.c

--- etdm/main.c
+++ etdm/main.c
@@ -13,7 +13,7 @@ int main(){
     F(a)F(b)F(c)F(d)
     for(i=1,*a*=-1,n--;n-i;a[i]/=b[i-1],b[i]-=a[i]*c[i-1],d[i]-=a[i]*d[i-1],a[i]*=-a[i-1],i++);
     --i,a[i]=(a[i]-c[i])/b[i],d[i]/=b[i];
-    for(--i;i+1;G(a)G(d)i--){}
+    for(--i;i+1;G(a)G(d)i--);
     a[n]=(d[n]-c[n]**d-a[n]*d[n-1])/(b[n]+a[n]*a[n-1]+c[n]**a);
     for(++i;n-i;printf("x%d=%f\n",i+1,d[i]+a[i]*a[n]),i++);
     printf("x%d=%f",++n,a[i]);
