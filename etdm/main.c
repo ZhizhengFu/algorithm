33eba89e6a0e9a5f320372b897d0e7b81f5552e2
Zhizheng Fu <zhizheng.foo@gmail.com>
2025-02-23 17:07:30 +0800

    Update main.c

--- etdm/main.c
+++ etdm/main.c
@@ -6,15 +6,15 @@
 // output: x1 x2 ... xn : solution
 #include <stdio.h>
 #define F(a)double a[n];for(i=0;i<n;scanf("%lf",a+i++));
+#define G(a)a[i]=(a[i]-c[i]*a[i+1])/b[i],
 int n,i;
 int main(){
     scanf("%d",&n);
     F(a)F(b)F(c)F(d)
-    *a*=-1,n--;
-    for(i=1;n-i;a[i]/=b[i-1],b[i]-=a[i]*c[i-1],d[i]-=a[i]*d[i-1],a[i]*=-a[i-1],i++);
-    i--,a[i]=(a[i]-c[i])/b[i],d[i]/=b[i];
-    for(--i;i+1;i--)a[i]=(a[i]-c[i]*a[i+1])/b[i],d[i]=(d[i]-c[i]*d[i+1])/b[i];
+    for(i=1,*a*=-1,n--;n-i;a[i]/=b[i-1],b[i]-=a[i]*c[i-1],d[i]-=a[i]*d[i-1],a[i]*=-a[i-1],i++);
+    --i,a[i]=(a[i]-c[i])/b[i],d[i]/=b[i];
+    for(--i;i+1;G(a)G(d)i--){}
     a[n]=(d[n]-c[n]**d-a[n]*d[n-1])/(b[n]+a[n]*a[n-1]+c[n]**a);
     for(++i;n-i;printf("x%d=%f\n",i+1,d[i]+a[i]*a[n]),i++);
-    printf("x%d=%f",n+1,a[i]);
+    printf("x%d=%f",++n,a[i]);
 }
